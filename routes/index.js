07eaec9434b79f7d7ec7472c8fe0897e2b9f7c02
Hayden Knicely <200570999@student.georgianc.on.ca>
2025-02-18 23:39:55 -0500

    Changes on contact

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,14 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Contact Page
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
 // Projects Page
 router.get('/projects', (req, res) => {
     res.render('projects');
 });
-
-// Contact Page
-router.post('/send-message', (req, res) => {
-    const { name, email, message } = req.body;
-    console.log(`New message from ${name} (${email}): ${message}`);
-
-    res.send('Message sent! (Not actually sending yet, just logging.)');
-});
-
 
 module.exports = router;
